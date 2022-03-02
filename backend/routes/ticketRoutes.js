579151a07a309a01486989ac50a7c479e87aa3aa
barbara.garbulha <barbara.garbulha@gmail.com>
2022-03-02 16:39:58 -0500

    creating tickets funcionality

--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -2,11 +2,20 @@ const express = require("express");
 const router = express.Router();
 const {
     getTickets,
-    createTickets,
+    getTicket,
+    createTicket,
+    deleteTicket,
+    updatedTicket,
 } = require("../controllers/ticketController");
 
 const { protect } = require("../middleware/authMiddleware");
 
 router.route("/").get(protect, getTickets).post(protect, createTicket);
+
+router
+    .route("/:id")
+    .get(protect, getTicket)
+    .delete(protect, deleteTicket)
+    .put(protect, updatedTicket);
 
 module.exports = router;
