495039f2fa37a60b451c5fd8f49ab20ef0afa2c4
barbara.garbulha <barbara.garbulha@gmail.com>
2022-03-04 13:54:47 -0500

    Prepare for deploy

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.use("/api/tickets", require("./routes/ticketRoutes"));
 //Serve Frontend
 if (process.env.NODE_ENV === "production") {
     //Set build folder as static
-    app.use(express.static(path.join(_dirname, "../frontend/build")));
+    app.use(express.static(path.join(__dirname, "../frontend/build")));
 
     app.get("*", (req, res) =>
         req.sendFile(__dirname, "../", "frontend", "build", "index.html")
